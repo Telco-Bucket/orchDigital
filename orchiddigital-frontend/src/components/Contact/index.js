bd18a367d0c32c8478c6c5cc66df2ed0bff6abcf
Samuel Egbajie (MAJ0R) <49847624+CodePapi@users.noreply.github.com>
2020-08-15 17:26:15 +0100

    contact form

--- orchiddigital-frontend/src/components/Contact/index.js
+++ orchiddigital-frontend/src/components/Contact/index.js
@@ -3,14 +3,14 @@ import NavBar from '../Layout/NavBar'
 import Footer from "../Layout/Footer"
 import Space from "../Home/Piece"
 import Form from "../Contact/ContactForm"
-
+import Map from "../Home/Map"
 export default function index() {
     return (
         <div style={{backgroundColor:"#F6F6F6"}}>
             <NavBar/>
             <Space/>
-            <Form Title="Contact Us"  showPurpose="none"/>
-
+            <Form />
+            <Map/>
            <Footer/> 
         </div>
     )
--- orchiddigital-frontend/src/components/Layout/Button.js
+++ orchiddigital-frontend/src/components/Layout/Button.js
@@ -5,7 +5,7 @@ export default function Button(props) {
     
     return (
         <div>
-             <input id="btn-start" type={props.Type} value={props.Text}  className="btn-start m-sm-1 m-xl-1 btn btn-start h1 " style={{width:"193px", height:"62px", border:"1px solid white", color:"white", background:"#3F3D56" }}/>
+             <input id="btn-start" type={props.Type} value={props.Text}  className="btn-start m-sm-1 m-xl-1 btn btn-start h1 " style={{maxWidth:"193px", height:"62px", border:"1px solid white", color:"white", background:"#3F3D56" }}/>
         </div>
     )
 }
