4d96c365d9ae5e7f6dc2921ae90c114cb2376764
Samuel Egbajie (MAJ0R) <49847624+CodePapi@users.noreply.github.com>
2020-09-06 20:06:44 +0100

    new start

--- orchiddigital-frontend/src/components/Home/Piece.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Piece(props) {
-    return (
-        <section style={{height:'90px', textAlign:"center", backgroundColor:props.bg?props.bg:"white"}}>
-        <h1>{props.text}</h1>
-        </section>
-    )
-}
